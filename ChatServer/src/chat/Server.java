dc32af472c88b3029ea70923a4cf7a8f0033655d
Ilya <boss.malinovskiy@mail.ru>
2017-12-11 22:46:05 +0400

    Update Server.java

--- ChatServer/src/chat/Server.java
+++ ChatServer/src/chat/Server.java
@@ -5,29 +5,17 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.logging.*;
 
 public class Server {
-    private static Logger logger = Logger.getLogger(Server.class.getName());
     static final int PORT = 3443;
     private ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
 
     public Server() {
-        try
-        {
-            FileHandler file = new FileHandler("MultiThreadServerSync.txt");
-            logger.addHandler(file);
-        }
-        catch (IOException e)
-        {
-            logger.log(Level.SEVERE, "Не удалось создать лог.", e);
-        }
         Socket clientSocket = null;
         ServerSocket serverSocket = null;
         try 
         {
             serverSocket = new ServerSocket(PORT);
-            logger.log(Level.INFO, "Сервер запущен. Используется порт = " + PORT);
             System.out.println("Сервер запущен.");
             while (true) {
                 clientSocket = serverSocket.accept();
@@ -43,12 +31,10 @@ public class Server {
         finally {
             try {
                 clientSocket.close();
-                logger.log(Level.INFO, "Сервер успешно остановлен");
                 System.out.println("Сервер успешно остановлен");
                 serverSocket.close();
             }
             catch (IOException ex) {
-                logger.log(Level.WARNING, "Не удалось успешно остановить сервер", e);
                 ex.printStackTrace();
             }
         }
